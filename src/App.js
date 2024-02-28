d00ac186a547a657025d31895dd2ec7556ce4841
Ilya Grishin <36699605+IlyaGrshin@users.noreply.github.com>
2024-02-29 01:17:07 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import Card from './Components/Card'
+import Card from './Components/Card';
 import ListItem from './Components/ListItem';
+import SectionHeader from './Components/SectionHeader';
 
 import './App.css';
 
@@ -7,14 +8,50 @@ function App() {
     return (
         <div>
             <Card>
+                Total Balance
+                $250.64
+            </Card>
+            <Card>
+                Send
+                Add Crypto
+                Exchange
+                Sell
+            </Card>
+            <Card>
                 <ListItem 
-                    bodyProps={{ label: 'Label' }}
+                    bodyProps={{ label: 'TON Space' }}
+                    key={1}
                 />
                 <ListItem 
-                    bodyProps={{ label: 'Label' }}
+                    bodyProps={{ label: 'Toncoin' }}
+                    key={2}
                 />
                 <ListItem 
-                    bodyProps={{ label: 'Label' }}
+                    bodyProps={{ label: 'Dollars' }}
+                    key={3}
+                />
+                <ListItem 
+                    bodyProps={{ label: 'Bitcoin' }}
+                    key={4}
+                />
+            </Card>
+            <Card>
+                <SectionHeader title='Transactions History' />
+                <ListItem 
+                    bodyProps={{ label: 'Alicia Torreaux' }}
+                    key={5}
+                />
+                <ListItem 
+                    bodyProps={{ label: 'Bob' }}
+                    key={6}
+                />
+                <ListItem 
+                    bodyProps={{ label: 'Purchased by Bank Card' }}
+                    key={7}
+                />
+                <ListItem 
+                    bodyProps={{ label: 'Ilya G' }}
+                    key={8}
                 />
             </Card>
         </div>
