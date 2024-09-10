358bd95d805739322981df991a9a3235a522c461
Ilya Grishin <36699605+IlyaGrshin@users.noreply.github.com>
2024-09-10 17:12:19 +0300

    Updated Router for TMA

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
-import { Routes, Route, useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
+import { Routes, Route } from 'react-router-dom';
 import { AnimatePresence } from 'framer-motion';
 import './index.css';
 
-import UI from './Pages/UI'
-import Wallet from './Pages/Wallet'
+import UI from './Pages/UI';
+import Wallet from './Pages/Wallet';
 
 function App() {
-  	const location = useLocation();
+    useEffect(() => {
+        const WebApp = window?.Telegram?.WebApp;
+		WebApp?.setBackgroundColor(WebApp.themeParams.secondary_bg_color)
+        WebApp?.setHeaderColor(WebApp.themeParams.secondary_bg_color);
+    }, []);
 
-	return (
-    	<AnimatePresence mode='wait' initial={false}>
-    		<Routes location={location} key={location.pathname}>
-    			<Route path='/' element={<UI />} />
-    			<Route path='/wallet' element={<Wallet />} />
-    		</Routes>
-    	</AnimatePresence>
-	);
+    return (
+        <AnimatePresence mode='wait' initial={false}>
+            <Routes>
+                <Route path='/' element={<UI />} />
+                <Route path='/wallet' element={<Wallet />} />
+                <Route path='*' element={<UI />} />
+            </Routes>
+        </AnimatePresence>
+    );
 }
 
 export default App;
