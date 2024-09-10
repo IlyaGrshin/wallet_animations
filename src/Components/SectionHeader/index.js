d6b6cffa8178d8457d74a42370d8a7754c128911
Ilya Grishin <36699605+IlyaGrshin@users.noreply.github.com>
2024-09-10 15:03:48 +0300

    Added new Section Headers

--- src/Components/SectionHeader/index.js
+++ src/Components/SectionHeader/index.js
@@ -1,19 +1,44 @@
 import Text from '../Text'
 import './index.css'
 
-function SectionHeader({ title, value }) {
-	return (
-		<div className='Section Headline'>
-			<Text variant='title3' weight='bold'>
-				{title}
-			</Text>
-			{value &&
-			<Text variant='title3' weight='bold'>
-				{value}
-			</Text>
-			}
-		</div>
-	)
+function SectionHeader({ type, title, value }) {
+	switch (type) {
+		case 'Headline':
+			return (
+				<div className="SectionHeader">
+					<Text variant='title3' weight='bold'>
+						{title}
+					</Text>
+					{value && (
+						<Text variant='title3' weight='bold'>
+							{value}
+						</Text>
+					)}
+				</div>
+			)
+		case 'Footer':
+			return (
+				<div className="SectionHeader Default">
+					<Text variant='footnote'>
+						{title}
+					</Text>
+				</div>
+			)
+		default:
+			return (
+				<div className="SectionHeader Default">
+					<Text variant='footnote' caps>
+						{title}
+					</Text>
+					{value && (
+						<Text variant='footnote'>
+							{value}
+						</Text>
+					)}
+				</div>
+			)
+	};
 }
+
 
 export default SectionHeader
--- src/Components/Text/index.js
+++ src/Components/Text/index.js
@@ -1,11 +1,13 @@
 import PropTypes from 'prop-types'
 import './index.css'
 
-const Text = ({ as: Component = 'div', variant, weight, rounded = false, children, ...props }) => {
+const Text = ({ as: Component = 'div', variant, weight, caps = false, rounded = false, children, ...props }) => {
     const dynamicProps = {
         ...(variant && { variant }),
         ...(weight && { weight }),
-    }
+        ...(rounded && { 'data-rounded': true }),
+        ...(caps && { 'data-caps': true }),
+    };
 
     return (
         <Component {...dynamicProps} {...props}>
@@ -15,9 +17,10 @@ const Text = ({ as: Component = 'div', variant, weight, rounded = false, childre
 }
 
 Text.propTypes = {
-    variant: PropTypes.oneOf(['title1', 'title3', 'body', 'subheadline2', 'subheadline2 rounded']),
+    variant: PropTypes.oneOf(['title1', 'title3', 'body', 'subheadline2', 'footnote']),
     weight: PropTypes.oneOf(['regular', 'medium', 'semibold', 'bold']),
-    rounded: PropTypes.bool
+    rounded: PropTypes.bool,
+    uppercase: PropTypes.bool
 }
 
 export default Text
