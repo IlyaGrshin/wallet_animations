b5001f1f803e1d0f4aaf693d37bc84cf55436076
Ilya Grishin <36699605+IlyaGrshin@users.noreply.github.com>
2024-09-16 20:03:41 +0300

    Changed Android to Material

--- src/Components/DeviceProvider/index.js
+++ src/Components/DeviceProvider/index.js
@@ -5,19 +5,19 @@ let platform = {
     'ios': 'apple',
     'macos': 'apple',
 
-    'android': 'android',
-    'android_x': 'android',
-    'tdesktop': 'android',
-    'webk': 'android',
-    'webz': 'android',
-    'unigram': 'android',
+    'android': 'material',
+    'android_x': 'material',
+    'tdesktop': 'material',
+    'webk': 'material',
+    'webz': 'material',
+    'unigram': 'material',
 }
 
 
 let telegramPlatform = WebApp.platform
 
 export const ios = platform[telegramPlatform] === 'apple';
-export const android = platform[telegramPlatform] === 'android';
+export const material = platform[telegramPlatform] === 'material';
 
 function DeviceProvider() {
     useEffect(() => {
