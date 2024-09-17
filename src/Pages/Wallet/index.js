c208200f02dfb91ba4d4cf2720dfacc24c9a0c44
Ilya Grishin <36699605+IlyaGrshin@users.noreply.github.com>
2024-09-17 11:28:36 +0300

    Huge Update for Assets Component

--- src/Pages/Wallet/index.js
+++ src/Pages/Wallet/index.js
@@ -150,12 +150,209 @@ function ActionButtons() {
     )
 }
 
+function AnimatedCell({ expandedAssets, index }) {
+
+    const transition = { ease: [0.26, 0.08, 0.25, 1], duration: 0.2 };
+
+    const logos = apple
+    ? [
+        {
+            src: ToncoinLogo,
+            zIndex: 2,
+            variants: {
+                collapsed: { scale: 0.6, top: '-6px', left: '-6px' },
+                expanded: { scale: 1, top: 0, left: 0 },
+            },
+        },
+        {
+            src: DollarsLogo,
+            zIndex: 1,
+            variants: {
+                collapsed: { scale: 0.6, opacity: 1, top: '6px', left: '6px' },
+                expanded: { scale: 0, opacity: 0, top: 0, left: 0 },
+            },
+        },
+    ]
+    : [
+        {
+            src: ToncoinLogo,
+            zIndex: 2,
+            variants: {
+                collapsed: { scale: 0.6, top: '-6px', left: 0 },
+                expanded: { scale: 1, top: 0, left: '6px' },
+            },
+        },
+        {
+            src: DollarsLogo,
+            zIndex: 1,
+            variants: {
+                collapsed: { scale: 0.6, opacity: 1, top: '6px', left: '12px' },
+                expanded: { scale: 0, opacity: 0, top: 0, left: '18px' },
+            },
+        },
+    ];
+
+    const logosStyle = apple
+    ? { position: 'relative', width: '40px', height: '40px' }
+    : { position: 'relative', width: '42px', height: '42px', marginLeft: '-6px' }
+
+    const animatedVariants = apple
+    ? {
+        moreAssets: {
+          collapsed: { opacity: 1, top: 'calc(50% - 11px)' },
+          expanded: { opacity: 0, top: 'calc(50% - 20px)' },
+        },
+        bodyTitle: {
+          collapsed: { opacity: 0, top: 'calc(50% - 11px)' },
+          expanded: { opacity: 1, top: 'calc(50% - 20px)' },
+        },
+        bodySubtitle: {
+          collapsed: { opacity: 0, top: 'calc(50% + 11px)' },
+          expanded: { opacity: 1, top: 'calc(50% + 2px)' },
+        },
+        endTitle1: {
+          collapsed: { opacity: 0, top: 'calc(50% - 2px)', right: '16px' },
+          expanded: { opacity: 1, top: 'calc(50% - 11px)', right: '16px' },
+        },
+        endTitle2: {
+          collapsed: { opacity: 1, top: 'calc(50% - 11px)', right: '16px' },
+          expanded: { opacity: 0, top: 'calc(50% - 20px)', right: '16px' },
+        },
+    }
+    : {
+        moreAssets: {
+          collapsed: { opacity: 1, top: 'calc(50% - 10px)' },
+          expanded: { opacity: 0, top: 'calc(50% - 20px)' },
+        },
+        bodyTitle: {
+          collapsed: { opacity: 0, top: 'calc(50% - 10px)' },
+          expanded: { opacity: 1, top: 'calc(50% - 20px)' },
+        },
+        bodySubtitle: {
+          collapsed: { opacity: 0, top: 'calc(50% + 12px)' },
+          expanded: { opacity: 1, top: 'calc(50% + 3px)' },
+        },
+        endTitle1: {
+          collapsed: { opacity: 0, top: 'calc(50% - 3px)', right: '20px' },
+          expanded: { opacity: 1, top: 'calc(50% - 10px)', right: '20px' },
+        },
+        endTitle2: {
+          collapsed: { opacity: 1, top: 'calc(50% - 10px)', right: '20px' },
+          expanded: { opacity: 0, top: 'calc(50% - 20px)', right: '20px' },
+        },
+    };
+
+    return (
+        <motion.div 
+            className='asset'
+            style={{ zIndex: 10 - index }}
+        >
+            <div className='Cell'>
+                <div className='start'>
+                    <div 
+                        className="assetIcon"
+                        style={logosStyle}
+                    >
+                        {logos.map((logo, index) => (
+                            <motion.div
+                                className='image' 
+                                variants={logo.variants}
+                                transition={transition}
+                                animate={expandedAssets ? 'expanded' : 'collapsed'}
+                                style={{ backgroundImage: `url(${logo.src})`, position: 'absolute', zIndex: logo.zIndex }}
+                                key={`stack-asset-${index}`}
+                            ></motion.div>
+                        ))}
+                    </div>
+                </div>
+                <div 
+                    className='body'
+                    style={{ position: 'relative' }}
+                >
+                    <motion.div
+                        variants={animatedVariants.moreAssets}
+                        transition={transition}
+                        animate={expandedAssets ? 'expanded' : 'collapsed'}
+                        style={{ transformOrigin: '0% 50%', position: 'absolute' }}
+                    >
+                        <Text 
+                            apple={{ variant: 'body', weight: 'medium' }}
+                            material={{ variant: 'body1', weight: 'medium' }}
+                        >
+                            More Assets
+                        </Text>
+                    </motion.div>
+                    <motion.div
+                        variants={animatedVariants.bodyTitle}
+                        transition={transition}
+                        animate={expandedAssets ? 'expanded' : 'collapsed'}
+                        style={{ transformOrigin: '0% 50%', position: 'absolute' }}
+                    >
+                        <Text 
+                            apple={{ variant: 'body', weight: 'medium' }}
+                            material={{ variant: 'body1', weight: 'medium' }}
+                            className='label'
+                        >
+                            Toncoin
+                        </Text>
+                    </motion.div>
+                    <motion.div
+                        variants={animatedVariants.bodySubtitle}
+                        transition={transition}
+                        animate={expandedAssets ? 'expanded' : 'collapsed'}
+                        style={{ transformOrigin: '0% 50%', position: 'absolute' }}
+                    >
+                        <Text 
+                            apple={{ variant: 'subheadline2', weight: 'regular' }}
+                            material={{ variant: 'subtitle2', weight: 'regular' }}
+                            className='caption' 
+                        >
+                            100 TON
+                        </Text>
+                    </motion.div>
+                </div>
+                <div className='end'>
+                    <motion.div
+                        variants={animatedVariants.endTitle1}
+                        transition={transition}
+                        animate={expandedAssets ? 'expanded' : 'collapsed'}
+                        style={{ transformOrigin: '0% 50%', position: 'absolute' }}
+                    >
+                        <Text 
+                            apple={{ variant: 'body', weight: 'regular' }}
+                            material={{ variant: 'body1', weight: 'regular' }}
+                            className='label'
+                        >
+                            $150.00
+                        </Text>
+                    </motion.div>
+                    <motion.div
+                        variants={animatedVariants.endTitle2}
+                        transition={transition}
+                        animate={expandedAssets ? 'expanded' : 'collapsed'}
+                        style={{ transformOrigin: '0% 50%', position: 'absolute' }}
+                    >
+                        <Text 
+                            apple={{ variant: 'body', weight: 'regular' }}
+                            material={{ variant: 'body1', weight: 'regular' }}
+                            className='label'
+                        >
+                            $201.92
+                        </Text>
+                    </motion.div>
+                </div>
+            </div>
+        </motion.div>
+    )
+}
+
 function Assets() {
     const [expandedAssets, setExpandedAssets] = useState(false); 
 
     const assets = [
         { name: 'TON Space', coins: '0 TON', value: '$0.00', image: TonSpaceLogo },
-        { name: 'Toncoin', coins: '100 TON', value: '$150.00', image: ToncoinLogo },
+        { isAnimated: true },
+        // { name: 'Toncoin', coins: '100 TON', value: '$150.00', image: ToncoinLogo },
         { name: 'Dollars', coins: '50 USDT', value: '$50.00', image: DollarsLogo },
         { name: 'Bitcoin', coins: '0.000011 BTC', value: '$50.64', image: BitcoinLogo },
         { name: '1 Bitcoin', coins: '0.000011 BTC', value: '$50.64', image: BitcoinLogo },
@@ -189,8 +386,10 @@ function Assets() {
 
         switch (index) {
             case 0:
+                return baseVariant
             case 1:
-                return baseVariant;
+                return {}
+                // return baseVariant;
             case 2:
                 return {
                     ...baseVariant,
@@ -244,10 +443,6 @@ function Assets() {
         ? { type: 'spring', stiffness: 420, damping: 28 }
         : { type: 'spring', stiffness: 800, damping: 60, mass: 1 }
 
-    const tapValue = apple
-        ? { scale: 0.99 }
-        : { scale: 1 }
-
     return (
         <AnimatePresence initial={false}>
             <motion.div
@@ -256,34 +451,46 @@ function Assets() {
                 onClick={toggleAssets}
             >
                 <Card className="assets">
-                    {assets.map((asset, index) => (
-                    <motion.div
-                        className="asset"
-                        key={`asset-${index}`}
-                        animate={getItemVariant(index)}
-                        transition={springValue}
-                        whileTap={tapValue}
-                    >
-                        <motion.div
-                            className="assetColorFill"
-                            animate={getAssetColorFill(index)}
-                            transition={{ ease: 'linear', duration: 0.15 }}
-                        />
-                        <motion.div
-                            animate={getItemVariantInside(index)}
-                            transition={{ ease: 'linear', duration: 0.15 }}
-                            style={{ position: 'relative' }}
-                        >
-                        <Cell
-                            start={<Cell.Start type="Image" src={asset.image} />}
-                            end={<Cell.Text title={asset.value} />}
-                            key={`tx-${index}`}
-                        >
-                            <Cell.Text title={asset.name} description={asset.coins} bold />
-                        </Cell>
-                        </motion.div>
-                    </motion.div>
-                    ))}
+                    {assets.map((asset, index) => {
+                        if (asset.isAnimated) {
+                            return (
+                                <AnimatedCell 
+                                    key={`animated-cell-${index}`}
+                                    expandedAssets={expandedAssets}
+                                    toggleAssets={toggleAssets}
+                                    index={index}
+                                />
+                            )
+                        }
+
+                        return(
+                            <motion.div
+                                className="asset"
+                                key={`asset-${index}`}
+                                animate={getItemVariant(index)}
+                                transition={springValue}
+                            >
+                                <motion.div
+                                    className="assetColorFill"
+                                    animate={getAssetColorFill(index)}
+                                    transition={{ ease: 'linear', duration: 0.15 }}
+                                />
+                                <motion.div
+                                    animate={getItemVariantInside(index)}
+                                    transition={{ ease: 'linear', duration: 0.15 }}
+                                    style={{ position: 'relative' }}
+                                >
+                                <Cell
+                                    start={<Cell.Start type="Image" src={asset.image} />}
+                                    end={<Cell.Text title={asset.value} />}
+                                    key={`tx-${index}`}
+                                >
+                                    <Cell.Text title={asset.name} description={asset.coins} bold />
+                                </Cell>
+                                </motion.div>
+                            </motion.div>
+                        )
+                    })}
                 </Card>
             </motion.div>
         </AnimatePresence>
