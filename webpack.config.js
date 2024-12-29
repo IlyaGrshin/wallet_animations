fb9e4863958715a44d74e0823e472e66ef4aa91b
Ilya Grishin <36699605+IlyaGrshin@users.noreply.github.com>
2024-12-29 23:46:59 +0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,7 @@ module.exports = {
 			use: {
 				loader: 'babel-loader',
 				options: {
-					configFile: path.resolve(__dirname, '.babelrc'),
+					configFile: path.resolve(__dirname, 'babel.config.js'),
 				},
 			},
 		},
