514d605067cf877d75c2e50d8787c54643f87e6e
Ilya Grishin <36699605+IlyaGrshin@users.noreply.github.com>
2025-02-20 14:03:24 +0300

    Update Component Name

--- src/index.js
+++ src/index.js
@@ -2,15 +2,15 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import "./index.css"
 import App from "./App"
-import ThemeProvider from "./components/ThemeProvider"
+import AppearanceProvider from "./components/AppearanceProvider"
 import DeviceProvider from "./components/DeviceProvider"
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
     <React.StrictMode>
         <DeviceProvider />
-        <ThemeProvider>
+        <AppearanceProvider>
             <App />
-        </ThemeProvider>
+        </AppearanceProvider>
     </React.StrictMode>
 )
