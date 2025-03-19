155843fc44c1e483d4ff2cc1b65b22be8bd32be7
Ilya Grishin <36699605+IlyaGrshin@users.noreply.github.com>
2025-03-20 01:09:19 +0300

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,38 +3,38 @@ import prettierPlugin from 'eslint-plugin-prettier';
 import babelParser from '@babel/eslint-parser';
 
 export default [
-  {
-    ignores: ['build/', 'node_modules/'], // Игнорируемые файлы
-    files: ['src/**/*.{js,jsx,ts,tsx}'], // Проверяемые файлы
-    languageOptions: {
-      ecmaVersion: 'latest', // Версия ECMAScript
-      sourceType: 'module', // Используем модули
-      parser: babelParser, // Используем Babel Parser
-      parserOptions: {
-        requireConfigFile: true, // Включение поддержки .babelrc
-        babelOptions: {
-          rootMode: 'upward', // Поддержка корневого файла Babel (если проект монорепозиторий)
+    {
+        ignores: ['build/', 'node_modules/'],
+        files: ['src/**/*.{js,jsx,ts,tsx}'],
+        languageOptions: {
+            ecmaVersion: 'latest',
+            sourceType: 'module',
+            parser: babelParser,
+            parserOptions: {
+                requireConfigFile: true,
+                babelOptions: {
+                    rootMode: 'upward',
+                },
+            },
         },
-      },
+        settings: {
+            react: {
+                version: 'detect',
+            },
+        },
+        plugins: {
+            react: reactPlugin,
+            prettier: prettierPlugin,
+        },
+        rules: {
+            ...reactPlugin.configs.recommended.rules,
+            'react/jsx-uses-vars': 'error',
+            'prettier/prettier': 'error',
+            'react/react-in-jsx-scope': 'off',
+            'react/prop-types': 'off',
+            'react/display-name': 'off',
+            'react/jsx-key': 'error',
+            'no-console': ['warn', { allow: ['warn', 'error'] }],
+        },
     },
-    settings: {
-      react: {
-        version: 'detect', // Автоопределение версии React
-      },
-    },
-    plugins: {
-      react: reactPlugin, // Плагин React
-      prettier: prettierPlugin, // Плагин Prettier
-    },
-    rules: {
-      ...reactPlugin.configs.recommended.rules, // Рекомендованные правила React
-      'react/jsx-uses-vars': 'error', // Поддержка используемых переменных
-      'prettier/prettier': 'error', // Интеграция Prettier
-      'react/react-in-jsx-scope': 'off', // Отключение правила
-      'react/prop-types': 'off', // Отключение правила
-      'react/display-name': 'off', // Отключение правила
-      'react/jsx-key': 'error',
-      'no-console': ['warn', { allow: ['warn', 'error'] }],
-    },
-  },
 ];
