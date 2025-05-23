fa4d6291b7ef77927ac61971be1e9aabeed80d0a
Ilya Grishin <36699605+IlyaGrshin@users.noreply.github.com>
2025-05-24 01:59:17 +0300

    Update stylelint.config.js

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,4 +1,13 @@
 /** @type {import('stylelint').Config} */
 export default {
   extends: ['stylelint-config-standard-scss'],
+  rules: {
+    'selector-pseudo-class-no-unknown': [true, {
+      ignorePseudoClasses: ['global'],
+    }],
+    'selector-class-pattern': [
+      '^(?:[a-z][a-z0-9]*(?:-[a-z0-9]+)*|[A-Z][a-zA-Z0-9]*)$',
+      { message: 'Разрешён kebab- или PascalCase' },
+    ],
+  },
 };
