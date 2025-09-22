6603dff64005a137e4fe9e244bbe9a5283ecb18d
Ilya Grishin <36699605+IlyaGrshin@users.noreply.github.com>
2025-09-22 15:48:58 +0300

    added react plugins

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,6 @@
 import reactPlugin from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import react19 from 'eslint-plugin-react-19-upgrade';
 import prettierPlugin from 'eslint-plugin-prettier';
 import babelParser from '@babel/eslint-parser';
 
@@ -24,10 +26,20 @@ export default [
         },
         plugins: {
             react: reactPlugin,
+            'react-hooks': reactHooks,
+            'react-19-upgrade': react19,
             prettier: prettierPlugin,
         },
         rules: {
             ...reactPlugin.configs.recommended.rules,
+            
+            ...(reactHooks.configs?.recommended?.rules ?? {}),
+
+            'react-hooks/rules-of-hooks': 'error',
+            'react-hooks/exhaustive-deps': 'warn',
+
+            ...(react19.configs?.recommended?.rules ?? {}),
+
             'react/jsx-uses-vars': 'error',
             'prettier/prettier': 'error',
             'react/react-in-jsx-scope': 'off',
--- src/components/TabBar/components/GradientMask/index.js
+++ src/components/TabBar/components/GradientMask/index.js
@@ -1,4 +1,4 @@
-import React, { useId } from "react"
+import { useId } from "react"
 import * as styles from "../../TabBar.module.scss"
 
 export default function GradientMask({
