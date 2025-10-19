2a167ce8ffe93d22a549846181609f598e172e5a
Ilya Grishin <36699605+IlyaGrshin@users.noreply.github.com>
2025-10-19 23:22:31 +0300

    Added GlassBorder to the button

--- src/components/Button/RegularButton/index.js
+++ src/components/Button/RegularButton/index.js
@@ -1,6 +1,9 @@
+import { GlassBorder } from "../../GlassEffect"
+import { motion } from "motion/react"
 import Text from "../../Text"
 
 import * as styles from "./RegularButton.module.scss"
+import { useApple } from "../../../hooks/DeviceProvider"
 
 export const RegularButton = ({
     variant,
@@ -15,17 +18,19 @@ export const RegularButton = ({
     }
 
     return (
-        <div
+        <motion.div
             className={`${styles.button} ${styles[variant]}`}
+            {...(useApple && { whileTap: { scale: 1.02 } })}
             {...dynamicProps}
             {...props}
         >
+            {(variant === "filled" || variant === "tinted") && <GlassBorder />}
             <Text
                 apple={{ variant: "body", weight: "semibold" }}
                 material={{ variant: "button1" }}
             >
                 {label}
             </Text>
-        </div>
+        </motion.div>
     )
 }
